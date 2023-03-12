751527e265ed1519316b702efccf93174b6c2df3
Ligamer24 <leandro200552@gmail.com>
2023-03-12 07:17:02 +0000

    Creating Leaderboard

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,16 +1,28 @@
+import "../Leaderboard.css"
+
 
 export default function Leaderboard() {
     return (
+        <>
+        <h1 id="title">Leaderboard</h1>
         <table>
+            
             <tbody>
+                <tr className="headRow">
+                    <th><h1>#</h1></th>
+                    <th><h1>Nome</h1></th>
+                    <th><h1>Turma</h1></th>
+                    <th><h1>Pontos</h1></th>
+                </tr>
                 <tr>
-                    <th>#</th>
-                    <th>Nome</th>
-                    <th>Turma</th>
-                    <th>Pontos</th>
+                    <td>1</td>
+                    <td>Ligamer24</td>
+                    <td>3ºSIS</td>
+                    <td>0</td>
                 </tr>
             </tbody>
         </table>
+        </>
     )
 }
 
