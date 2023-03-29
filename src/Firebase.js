314181c2ea0aa8a1378c1e66ad52c8eb4b1381e0
Ligamer24 <leandro200552@gmail.com>
2023-03-29 17:18:26 +0100

    teste

--- src/Firebase.js
+++ src/Firebase.js
@@ -37,8 +37,8 @@ export async function getUsers() {
 export async function addUserToDb(user) {
   var userClass = user.displayName.split(" ")
   userClass = userClass[userClass.length-1]
-  alert("Insira o seu username:")
-  const userDesiredName = prompt("")
+  window.focus()
+  const userDesiredName = prompt("Insira o seu username:")
   user.displayName = userDesiredName
   await setDoc(doc(db, "users", user.uid), {
     Class: userClass,
